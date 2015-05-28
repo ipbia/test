ac6f87bb56dfaa3ad7c7ad63c51b96f3ce2e1c4b
31by <https://github.com/ipbia/test.git>
2015-05-28 15:22:58 +0800

    新增婚庆二维码

---
+++ PigCms/Lib/Action/User/QrCodeAction.class.php
@@ -0,0 +1,16 @@
+<?php
+class QrCodeAction extends BaseAction{
+	
+	public	function generateQRForAjax()
+	{
+		$chl = 'http://'.$_SERVER['HTTP_HOST'];
+		if(isset($_POST['chl'])){
+			$chl .= $_POST['chl'];
+		}
+		echo '<img src="http://api.k780.com:88/?app=qr.get&data='.urlencode($chl).'&level=L&size=10"/>';
+	}
+	
+	
+}
+	
+?>
